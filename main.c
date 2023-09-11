11570243fa3e7858d88d36efbb1167e8dfc330f0
Salah eddine Lazar <slazar@c1r5p3.1337.ma>
2023-09-11 17:32:23 +0100

    f

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: slazar <slazar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 19:24:41 by slazar            #+#    #+#             */
-/*   Updated: 2023/09/11 13:36:03 by slazar           ###   ########.fr       */
+/*   Updated: 2023/09/11 16:43:26 by slazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,15 @@
 
 int only_sp_tab(char *line)
 {
-	while (*line)
-	{
-		if(*line != ' ' || *line != '\t')
-			return (1);
+	while (*line && ( *line == ' ' || *line == '\t'))
 		line++;
-	}
-	return(0);
+	return(*line ? 1 : 0);
 }
 int check_space(char *line)
 {
-	if(*line == '\0' || only_sp_tab(line))
-		return(1);
-	return(0);
+	if(*line == '\0' || only_sp_tab(line) == 0)
+		return(0);
+	return(1);
 }
 // int syntax_error(char *line)
 // {
@@ -51,15 +47,17 @@ int main(int __unused ac,char **av,char **envirement)
 		line = readline("minishell ");
 		if (!line)
 			printf("Error\n");
-		else if (check_space(line))
+		else if (check_space(line) == 0)
 			continue;
 		else if(!strcmp(line,"env"))
             print_env(env);
 		else
+		{
 			add_history(line);
 			lexer(line, &lx);
 			ft_print_lexer(&lx.head);
 		}
+	}
 	if(line)
 		free(line);
 }
